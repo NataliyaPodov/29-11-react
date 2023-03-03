6d902c9357fa2332306063edcbdcbf0d472ca809
NataliyaPodov <natapol84@ukr.net>
2023-03-03 14:04:16 +0200

    add h1 and 2p

--- src/index.tsx
+++ src/index.tsx
@@ -1,47 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-/* const title = React.createElement(
-    'h1',
-    {
-        id: 'title',
-        className: 'red',
-    },
-    'Hello React'
-) */
-
-/* With JSX */
-
-const title = <h1 className="red">Test</h1>
-
-const list = (
-    <ul>
-        <li>List item 1</li>
-        <li>List item 2</li>
-        <li>List item 3</li>
-    </ul>
-)
-
-const content = (
-    <div>
-        {title}
-        {list}
-    </div>
-)
-
-/* Реакт компонент */
-
-/* function App() {
-    return <h1>Hello App component</h1>
-} */
-
 /* Стрелочная функция */
 
 const App = () => {
-    return <h1>Hello App component</h1>
+    return (
+        <div>
+            {' '}
+            <h1>Hello App component</h1>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Numquam consequuntur voluptatibus vel doloremque placeat
+                debitis? Dolor minima, amet optio tempore consequatur vitae
+                voluptatum pariatur mollitia dolorem tempora, adipisci, voluptas
+                delectus?
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
+                eos nihil tempore, veniam harum reprehenderit, voluptatem quod
+                neque magnam amet culpa suscipit minima delectus necessitatibus
+                eaque deleniti, ipsa nisi. Itaque!
+            </p>
+        </div>
+    )
 }
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-/* root.render(<React.StrictMode>{content}</React.StrictMode>) */
+
 root.render(
     <React.StrictMode>
         <App />
