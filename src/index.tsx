e97daa63f5d3b84c9cd1502b28467592f4c782e9
NataliyaPodov <natapol84@ukr.net>
2023-03-03 18:05:42 +0200

    add props Content

--- src/index.tsx
+++ src/index.tsx
@@ -1,34 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Title from './Title'
+import Content from './Content'
 
 /* Стрелочная функция */
 
-const Content = () => {
-    return (
-        <React.Fragment>
-            <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Numquam consequuntur voluptatibus vel doloremque placeat
-                debitis? Dolor minima, amet optio tempore consequatur vitae
-                voluptatum pariatur mollitia dolorem tempora, adipisci, voluptas
-                delectus?
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
-                eos nihil tempore, veniam harum reprehenderit, voluptatem quod
-                neque magnam amet culpa suscipit minima delectus necessitatibus
-                eaque deleniti, ipsa nisi. Itaque!
-            </p>
-        </React.Fragment>
-    )
-}
 const App = () => {
     return (
         <>
             <Title title="React" year={2023} />
             <Title title="App" year={2022} />
-            <Content />
+            <Content text1="Hello world text1" text2="Hello world text2" />
         </>
     )
 }
