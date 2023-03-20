ba0bf161bc2cdf34c3d355d1e2a78da8286bdf3e
NataliyaPodov <natapol84@ukr.net>
2023-03-20 14:12:47 +0200

    при натисканні на кнопку додається в корзину кількість та ціна продуктів

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -18,11 +18,18 @@ const App = (props: Props) => {
         totalPrice: 0,
     })
 
+    const addProductToCart = (count: number, price: number) => {
+        setCartData((prevState) => ({
+            totalCount: prevState.totalCount + count,
+            totalPrice: prevState.totalPrice + count * price,
+        }))
+    }
+
     return (
         <StyledEngineProvider injectFirst>
             <CssBaseline />
             <Header cartData={cartData} />
-            <Main />
+            <Main addProductToCart={addProductToCart} />
             <Footer />
         </StyledEngineProvider>
     )
